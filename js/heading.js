4432ab59f7126a238d878100fbd47059bde89a2a
Daniel Yu <dyu60281@usc.edu>
2023-05-20 11:30:31 -0700

    Finished resume and about page

--- js/heading.js
+++ js/heading.js
@@ -9,3 +9,32 @@ function loadResume() {
 function loadAbout() {
     window.location.href = "about.html";
 }
+
+function loadMode() {
+    var body = document.body;
+    if (sessionStorage.getItem("lightmode") == null) {
+        sessionStorage.setItem("lightmode", "false");
+    }
+
+    if (sessionStorage.getItem("lightmode") == "true") {
+        body.classList.toggle("light-mode", "true");
+    }
+    else {
+        body.classList.toggle("light-mode", "false");
+    }
+}
+
+function darkMode() {
+    var body = document.body;
+    body.classList.toggle("light-mode");
+
+    var head = document.getElementById("darkMode");
+    if (head.innerHTML == "Dark Mode") {
+        head.innerHTML = "Light Mode"
+        sessionStorage.setItem("lightmode", "false");
+    }
+    else {
+        head.innerHTML = "Dark Mode"
+        sessionStorage.setItem("lightmode", "true");
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,16 +1,30 @@
-var data = '[{"name": "test game", "roles": "designer", "page": "test.html"},{"name": "test game2", "roles": "designer2", "page": "test2.html"}]';
+var data = '['
+    + '{"name": "test game", "roles": "designer", "page": "test.html"}'
+    + ',{"name": "test game2", "roles": "designer2", "page": "test2.html"}'
+    + ',{"name": "test game3", "roles": "designer2", "page": "test2.html"}'
+    + ',{"name": "test game4", "roles": "designer2", "page": "test2.html"}'
+    + ']';
 
 
 function loadPortfolio() {
     var json = JSON.parse(data);
-    var temp = "";
-    for (var i in json) {
-        temp += "<div class=\"hoverbox\" onclick=\"loadItem('" + json[i].page + "')\">"
-        temp += json[i].name + " ";
-        temp += json[i].roles + " ";
-        temp += "</div>";
-        temp += "<br/>";
+    var temp = "<table style=\"width: 100%\">";
+    for (var i = 0; i < json.length; i += 3) {
+        temp += "<tr>";
+        for (var j = i; j < i + 3; j++) {
+            temp += "<td width=\"33%\">";
+            if (j < json.length) {
+                temp += "<div class=\"hoverbox\" onclick=\"loadItem('" + json[j].page + "')\">"
+                temp += json[j].name + " ";
+                temp += json[j].roles + " ";
+                temp += "</div>";
+                temp += "<br/>";
+            }
+            temp += "</td>"
+        }
+        temp += "</tr>";
     }
+    temp += "</table>";
     document.getElementById("portfolio").innerHTML = temp;
 }
 
