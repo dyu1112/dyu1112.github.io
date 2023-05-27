73a3abfb966b38fc5d353db5c0ea1b0c6dd9a7b0
Daniel Yu <dyu60281@usc.edu>
2023-05-26 21:31:50 -0700

    Added categorization

--- js/index.js
+++ js/index.js
@@ -1,21 +1,26 @@
 var data = '['
-    + '{"name": "USCheduler", "roles": "Backend Developer", "page": "uscheduler.html", "img": "assets/uscheduler_preview.png"}'
-    + ',{"name": "Hear Your Fear", "roles": "Programmer, Designer, 3D Artist", "page": "hearyourfear.html", "img": "assets/hearyourfear_preview.png"}'
-    + ',{"name": "Bone Boy", "roles": "Composer, Sound Designer, Audio Programmer", "page": "boneboy.html", "img": "assets/boneboy_preview.png"}'
-    + ',{"name": "Cellulose", "roles": "Composer, Programmer, UI", "page": "cellulose.html", "img": "assets/cellulose_preview.jpg"}'
-    + ',{"name": "Mom, Please Respect My Privacy", "roles": "Sound Designer, Audio Programmer", "page": "mprmp.html", "img": "assets/mprmp_preview.png"}'
-    + ',{"name": "Ho Ho Who Did It?", "roles": "Audio Designer", "page": "hhwdi.html", "img": "assets/hhwdi_preview.jpg"}'
+    + '{"name": "USCheduler", "roles": "Backend Developer", "page": "uscheduler.html", "img": "assets/uscheduler_preview.png", "category": "Software"}'
+    + ',{"name": "Hear Your Fear", "roles": "Programmer, Designer, 3D Artist", "page": "hearyourfear.html", "img": "assets/hearyourfear_preview.png", "category": "Games"}'
+    + ',{"name": "Bone Boy", "roles": "Composer, Sound Designer, Audio Programmer", "page": "boneboy.html", "img": "assets/boneboy_preview.png", "category": "Games"}'
+    + ',{"name": "Cellulose", "roles": "Composer, Programmer, UI", "page": "cellulose.html", "img": "assets/cellulose_preview.jpg", "category": "Games"}'
+    + ',{"name": "Mom, Please Respect My Privacy", "roles": "Sound Designer, Audio Programmer", "page": "mprmp.html", "img": "assets/mprmp_preview.png", "category": "Games"}'
+    + ',{"name": "Ho Ho Who Did It?", "roles": "Audio Designer", "page": "hhwdi.html", "img": "assets/hhwdi_preview.jpg", "category": "Games"}'
     + ']';
 
+var currentCategory = "All";
 
-function loadPortfolio() {
+function loadPortfolio(category) {
     var json = JSON.parse(data);
     var temp = "<table style=\"width: 100%\">";
     for (var i = 0; i < json.length; i += 3) {
         temp += "<tr>";
         for (var j = i; j < i + 3; j++) {
+            if (j < json.length && json[j].category != category && category != "All") {
+                i++;
+                continue;
+            }
             temp += "<td width=\"33%\" class=\"portfolioItem\">";
-            if (j < json.length) {
+            if (j < json.length && (json[j].category == category || category == "All")) {
                 temp += "<div class=\"hoverbox\" onclick=\"loadItem('" + json[j].page + "')\">";
                 temp += "<img src=\"" + json[j].img + "\" width=\"100%\">";
                 temp += "<br /><br /><h3>" + json[j].name + "</h3>";
@@ -28,6 +33,9 @@ function loadPortfolio() {
     }
     temp += "</table>";
     document.getElementById("portfolio").innerHTML = temp;
+    document.getElementById(currentCategory).innerHTML = "<heading onclick=\"loadPortfolio('" + currentCategory + "')\">" + currentCategory + "</heading>";
+    document.getElementById(category).innerHTML = "<heading style=\"font-family: AdirBold, arial, sans-serif; color: var(--emerald)\" onclick=\"loadPortfolio('" + category + "')\">" + category + "</heading>";
+    currentCategory = category;
 }
 
 function loadItem(page) {
