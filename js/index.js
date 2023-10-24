5c9adeb0b417c156785ba7861f179ab0e8fabec0
Daniel Yu <dyu60281@usc.edu>
2023-10-24 13:27:15 -0700

    Implemented functionality for projects to have multiple categories. Added a Sound category.

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 var data = '['
-    + '{"name": "Free Will", "roles": "Programmer, Designer", "page": "freewill.html", "img": "assets/freewill_preview.png", "category": "Games"}'
-    + ',{"name": "Sorelle", "roles": "Sound Designer", "page": "sorelle.html", "img": "assets/sorelle_preview.png", "category": "Games"}'
-    + ',{"name": "USCheduler", "roles": "Backend Developer", "page": "uscheduler.html", "img": "assets/uscheduler_preview.png", "category": "Software"}'
-    + ',{"name": "Hear Your Fear", "roles": "Lead Programmer, Designer, 3D Artist", "page": "hearyourfear.html", "img": "assets/hearyourfear_preview.png", "category": "Games"}'
-    + ',{"name": "Bone Boy", "roles": "Composer, Sound Designer, Audio Programmer", "page": "boneboy.html", "img": "assets/boneboy_preview.png", "category": "Games"}'
-    + ',{"name": "Cellulose", "roles": "Composer, Programmer, UI", "page": "cellulose.html", "img": "assets/cellulose_preview.jpg", "category": "Games"}'
-    + ',{"name": "Mom, Please Respect My Privacy", "roles": "Sound Designer, Audio Programmer", "page": "mprmp.html", "img": "assets/mprmp_preview.png", "category": "Games"}'
-    + ',{"name": "Ho Ho Who Did It?", "roles": "Audio Designer", "page": "hhwdi.html", "img": "assets/hhwdi_preview.jpg", "category": "Games"}'
+    + '{"name": "Free Will", "roles": "Programmer, Designer", "page": "freewill.html", "img": "assets/freewill_preview.png", "category": ["Games"]}'
+    + ',{"name": "Sorelle", "roles": "Sound Designer", "page": "sorelle.html", "img": "assets/sorelle_preview.png", "category": ["Sound"]}'
+    + ',{"name": "USCheduler", "roles": "Backend Developer", "page": "uscheduler.html", "img": "assets/uscheduler_preview.png", "category": ["Software"]}'
+    + ',{"name": "Hear Your Fear", "roles": "Lead Programmer, Designer, 3D Artist", "page": "hearyourfear.html", "img": "assets/hearyourfear_preview.png", "category": ["Games", "Sound"]}'
+    + ',{"name": "Bone Boy", "roles": "Composer, Sound Designer, Audio Programmer", "page": "boneboy.html", "img": "assets/boneboy_preview.png", "category": ["Games", "Sound"]}'
+    + ',{"name": "Cellulose", "roles": "Composer, Programmer, UI", "page": "cellulose.html", "img": "assets/cellulose_preview.jpg", "category": ["Games", "Sound"]}'
+    + ',{"name": "Mom, Please Respect My Privacy", "roles": "Sound Designer, Audio Programmer", "page": "mprmp.html", "img": "assets/mprmp_preview.png", "category": ["Games", "Sound"]}'
+    + ',{"name": "Ho Ho Who Did It?", "roles": "Audio Designer", "page": "hhwdi.html", "img": "assets/hhwdi_preview.jpg", "category": ["Sound"]}'
     + ']';
 
 var currentCategory = "All";
@@ -17,12 +17,17 @@ function loadPortfolio(category) {
     for (var i = 0; i < json.length; i += 3) {
         temp += "<tr>";
         for (var j = i; j < i + 3; j++) {
-            if (j < json.length && json[j].category != category && category != "All") {
-                i++;
-                continue;
+            if (j < json.length) {
+                categories = json[j].category;
+                if (!categories.includes(category) && category != "All") {
+                    i++;
+                    continue;
+                }
+
             }
+
             temp += "<td width=\"33%\" class=\"portfolioItem\">";
-            if (j < json.length && (json[j].category == category || category == "All")) {
+            if (j < json.length && (categories.includes(category) || category == "All")) {
                 temp += "<div class=\"hoverbox\" onclick=\"loadItem('" + json[j].page + "')\">";
                 temp += "<img src=\"" + json[j].img + "\" width=\"100%\">";
                 temp += "<br /><br /><h3>" + json[j].name + "</h3>";
