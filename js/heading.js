496e64d80fa9288eb54e56d53d07b4be5306cff3
Daniel Yu <dyu60281@usc.edu>
2024-03-26 02:34:06 -0700

    Changed fonts

--- js/heading.js
+++ js/heading.js
@@ -9,32 +9,3 @@ function loadCV() {
 function loadAbout() {
     window.location.href = "about.html";
 }
-
-function loadMode() {
-    var body = document.body;
-    if (sessionStorage.getItem("lightmode") == null) {
-        sessionStorage.setItem("lightmode", "false");
-    }
-
-    if (sessionStorage.getItem("lightmode") == "true") {
-        body.classList.toggle("light-mode", "true");
-    }
-    else {
-        body.classList.toggle("light-mode", "false");
-    }
-}
-
-function darkMode() {
-    var body = document.body;
-    body.classList.toggle("light-mode");
-
-    var head = document.getElementById("darkMode");
-    if (head.innerHTML == "Dark Mode") {
-        head.innerHTML = "Light Mode"
-        sessionStorage.setItem("lightmode", "false");
-    }
-    else {
-        head.innerHTML = "Dark Mode"
-        sessionStorage.setItem("lightmode", "true");
-    }
-}
