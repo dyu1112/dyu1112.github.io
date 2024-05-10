4cb549eca3ce8a355e377d1ff847722e3242264f
Daniel Yu <dyu60281@usc.edu>
2024-05-10 00:41:01 -0700

    New background and header

--- js/heading.js
+++ js/heading.js
@@ -9,3 +9,14 @@ function loadCV() {
 function loadAbout() {
     window.location.href = "about.html";
 }
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementsByClassName("headerBox")[0].style.top = "0";
+    } else {
+        document.getElementsByClassName("headerBox")[0].style.top = "-100px";
+    }
+    prevScrollpos = currentScrollPos;
+}
