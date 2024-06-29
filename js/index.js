a2a157b29ccbc60ebb31af03ec9716f442a359f6
Daniel Yu <danielyu742@outlook.com>
2024-06-28 20:28:56 -0700

    Reskinned website

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ function loadPortfolio(category) {
             continue;
         }
         else {
-            temp += "<div class=\"hoverbox\" onclick=\"loadItem('" + data[i].page + "')\">";
+            temp += "<a class=\"hoverbox\" href=\"" + data[i].page + "\">";
             temp += "<img src=\"" + data[i].img + "\" width=\"100%\">";
             temp += "<br /><br /><h3>" + data[i].name + "</h3>";
             temp += "<p>" + data[i].roles + "</p>";
@@ -17,8 +17,9 @@ function loadPortfolio(category) {
         }
     }
     document.getElementById("portfolio").innerHTML = temp;
-    document.getElementById(currentCategory).innerHTML = "<heading onclick=\"loadPortfolio('" + currentCategory + "')\">" + currentCategory + "</heading>";
-    document.getElementById(category).innerHTML = "<heading style=\"font-family: RobotoBold, arial, sans-serif; color: var(--emerald)\" onclick=\"loadPortfolio('" + category + "')\">" + category + "</heading>";
+    document.getElementById(currentCategory).innerHTML = "<span class=\"categoryItem\" onclick=\"loadPortfolio('" + currentCategory + "')\">" + currentCategory + "</heading>";
+    document.getElementById(category).innerHTML = "<span class=\"categoryItem\" style=\"background-color: var(--orange)\" onclick=\"loadPortfolio('" + category + "')\">" + category + "</heading>";
+    document.getElementById(category).style
     currentCategory = category;
 }
 
--- js/portfolio.js
+++ js/portfolio.js
@@ -12,7 +12,7 @@ data = [
     "roles": "Sound Designer",
     "page": "sorelle.html",
     "img": "assets/sorelle_preview.png",
-    "category": [ "Sound" ],
+    "category": [ "Audio" ],
     "tech": "Wwise, Ableton Live, Sound Particles"
   },
   {
@@ -20,7 +20,7 @@ data = [
     "roles": "Producer, Programmer",
     "page": "sorcerersmackdown.html",
     "img": "assets/sorcerersmackdown_preview.png",
-    "category": [ "Game Programming", "Sound" ],
+    "category": [ "Game Programming", "Audio" ],
     "tech": "C#, Unity 2D, Wwise, Adobe Premiere Pro"
   },
   {
@@ -44,7 +44,7 @@ data = [
     "roles": "Programmer, Sound Designer",
     "page": "unnamedsurgeon.html",
     "img": "assets/unnamedsurgeon_preview.png",
-    "category": [ "Game Programming", "Sound" ],
+    "category": [ "Game Programming", "Audio" ],
     "tech": "C#, Unity 3D"
   },
   {
@@ -60,7 +60,7 @@ data = [
     "roles": "Sound Lead",
     "page": "trickortreat.html",
     "img": "assets/boosthere_preview.png",
-    "category": [ "Sound" ],
+    "category": [ "Audio" ],
     "tech": "C#, Unity 2D, Wwise, Yarn Spinner, Ableton Live"
   },
   {
@@ -84,7 +84,7 @@ data = [
     "roles": "Lead Programmer, Designer, 3D Artist",
     "page": "hearyourfear.html",
     "img": "assets/hearyourfear_preview.png",
-    "category": [ "Game Programming", "Sound" ],
+    "category": [ "Game Programming", "Audio" ],
     "tech": "C#, Unity 3D, Autodesk Maya"
   },
   {
@@ -92,7 +92,7 @@ data = [
     "roles": "Composer, Sound Designer, Audio Programmer",
     "page": "boneboy.html",
     "img": "assets/boneboy_preview.png",
-    "category": [ "Game Programming", "Sound" ],
+    "category": [ "Game Programming", "Audio" ],
     "tech": "C#, Unity 2D, Wwise, Ableton Live"
   },
   {
@@ -100,7 +100,7 @@ data = [
     "roles": "Composer, Programmer, UI",
     "page": "cellulose.html",
     "img": "assets/cellulose_preview.jpg",
-    "category": [ "Game Programming", "Sound" ],
+    "category": [ "Game Programming", "Audio" ],
     "tech": "C#, Unity 2D, Ableton Live"
   },
   {
@@ -108,7 +108,7 @@ data = [
     "roles": "Sound Designer, Audio Programmer",
     "page": "mprmp.html",
     "img": "assets/mprmp_preview.png",
-    "category": [ "Game Programming", "Sound" ],
+    "category": [ "Game Programming", "Audio" ],
     "tech": "C#, Unity 2D"
   },
   {
@@ -116,7 +116,7 @@ data = [
     "roles": "Audio Designer",
     "page": "hhwdi.html",
     "img": "assets/hhwdi_preview.jpg",
-    "category": [ "Sound" ],
+    "category": [ "Audio" ],
     "tech": "Ableton Live"
   }
 ];
