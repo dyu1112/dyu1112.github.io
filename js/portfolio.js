4bcb52e3513d54f5e58304ab82967086fa225311
Daniel Yu <dyu60281@usc.edu>
2024-07-02 23:59:05 -0700

    New preview images

--- js/portfolio.js
+++ js/portfolio.js
@@ -24,123 +24,123 @@ data = [
         "tech": "C#, Unity 3D, State Machines"
     },
     {
-    "name": "Free Will",
-    "roles": "Programmer, Designer",
-    "page": "freewill.html",
-    "img": "assets/freewill_preview.png",
-    "category": [ "Game Programming" ],
-    "tech": "C#, Unity 2D"
-  },
-  {
-    "name": "Sorelle",
-    "roles": "Sound Designer",
-    "page": "sorelle.html",
-    "img": "assets/sorelle_preview.png",
-    "category": [ "Audio" ],
-    "tech": "Wwise, Ableton Live, Sound Particles"
-  },
-  {
-    "name": "Sorcerer Smackdown",
-    "roles": "Producer, Programmer",
-    "page": "sorcerersmackdown.html",
-    "img": "assets/sorcerersmackdown_preview.png",
-    "category": [ "Game Programming", "Audio" ],
-    "tech": "C#, Unity 2D, Wwise, Adobe Premiere Pro"
-  },
-  {
-    "name": "Syncon",
-    "roles": "Programmer",
-    "page": "syncon.html",
-    "img": "assets/syncon_preview.png",
-    "category": [ "Game Programming" ],
-    "tech": "C#, Unity 2D"
-  },
-  {
-    "name": "Try Not To Hit [An Unspecified] Bridge",
-    "roles": "Programmer, Artist",
-    "page": "unspecifiedbridge.html",
-    "img": "assets/unspecifiedbridge_preview.png",
-    "category": [ "Game Programming" ],
-    "tech": "C#, Unity 3D, Autodesk Maya"
-  },
-  {
-    "name": "Unnamed Surgeon Game",
-    "roles": "Programmer, Sound Designer",
-    "page": "unnamedsurgeon.html",
-    "img": "assets/unnamedsurgeon_preview.png",
-    "category": [ "Game Programming", "Audio" ],
-    "tech": "C#, Unity 3D"
-  },
-  {
-    "name": "ITP 438 - Advanced Gameplay Programming",
-    "roles": "Programmer",
-    "page": "itp438.html",
-    "img": "assets/itp438_preview.jpg",
-    "category": [ "Game Programming" ],
-    "tech": "C++, Unreal Engine 5"
-  },
-  {
-    "name": "Boo's There?",
-    "roles": "Sound Lead",
-    "page": "trickortreat.html",
-    "img": "assets/boosthere_preview.png",
-    "category": [ "Audio" ],
-    "tech": "C#, Unity 2D, Wwise, Yarn Spinner, Ableton Live"
-  },
-  {
-    "name": "Portal Recreation",
-    "roles": "Programmer",
-    "page": "portalrecreation.html",
-    "img": "assets/portal_preview.png",
-    "category": [ "Game Programming" ],
-    "tech": "C++, SDL Library"
-  },
-  {
-    "name": "USCheduler",
-    "roles": "Backend Developer",
-    "page": "uscheduler.html",
-    "img": "assets/uscheduler_preview.png",
-    "category": [ "SWE" ],
-    "tech": "Java, Java Servlets, MySQL Workbench"
-  },
-  {
-    "name": "Hear Your Fear",
-    "roles": "Lead Programmer, Designer, 3D Artist",
-    "page": "hearyourfear.html",
-    "img": "assets/hearyourfear_preview.png",
-    "category": [ "Game Programming", "Audio" ],
-    "tech": "C#, Unity 3D, Autodesk Maya"
-  },
-  {
-    "name": "Bone Boy",
-    "roles": "Composer, Sound Designer, Audio Programmer",
-    "page": "boneboy.html",
-    "img": "assets/boneboy_preview.png",
-    "category": [ "Game Programming", "Audio" ],
-    "tech": "C#, Unity 2D, Wwise, Ableton Live"
-  },
-  {
-    "name": "Cellulose",
-    "roles": "Composer, Programmer, UI",
-    "page": "cellulose.html",
-    "img": "assets/cellulose_preview.jpg",
-    "category": [ "Game Programming", "Audio" ],
-    "tech": "C#, Unity 2D, Ableton Live"
-  },
-  {
-    "name": "Mom, Please Respect My Privacy",
-    "roles": "Sound Designer, Audio Programmer",
-    "page": "mprmp.html",
-    "img": "assets/mprmp_preview.png",
-    "category": [ "Game Programming", "Audio" ],
-    "tech": "C#, Unity 2D"
-  },
-  {
-    "name": "Ho Ho Who Did It?",
-    "roles": "Audio Designer",
-    "page": "hhwdi.html",
-    "img": "assets/hhwdi_preview.png",
-    "category": [ "Audio" ],
-    "tech": "Ableton Live"
-  }
+        "name": "Free Will",
+        "roles": "Programmer, Designer",
+        "page": "freewill.html",
+        "img": "assets/freewill_preview.png",
+        "category": ["Game Programming"],
+        "tech": "C#, Unity 2D"
+    },
+    {
+        "name": "Sorelle",
+        "roles": "Sound Designer",
+        "page": "sorelle.html",
+        "img": "assets/sorelle_preview.png",
+        "category": ["Audio"],
+        "tech": "Wwise, Ableton Live, Sound Particles"
+    },
+    {
+        "name": "Sorcerer Smackdown",
+        "roles": "Producer, Programmer",
+        "page": "sorcerersmackdown.html",
+        "img": "assets/sorcerersmackdown_preview.png",
+        "category": ["Game Programming", "Audio"],
+        "tech": "C#, Unity 2D, Wwise, Adobe Premiere Pro"
+    },
+    {
+        "name": "Syncon",
+        "roles": "Programmer",
+        "page": "syncon.html",
+        "img": "assets/syncon_preview.png",
+        "category": ["Game Programming"],
+        "tech": "C#, Unity 2D"
+    },
+    {
+        "name": "Try Not To Hit [An Unspecified] Bridge",
+        "roles": "Programmer, Artist",
+        "page": "unspecifiedbridge.html",
+        "img": "assets/unspecifiedbridge_preview.png",
+        "category": ["Game Programming"],
+        "tech": "C#, Unity 3D, Autodesk Maya"
+    },
+    {
+        "name": "Unnamed Surgeon Game",
+        "roles": "Programmer, Sound Designer",
+        "page": "unnamedsurgeon.html",
+        "img": "assets/unnamedsurgeon_preview.png",
+        "category": ["Game Programming", "Audio"],
+        "tech": "C#, Unity 3D"
+    },
+    {
+        "name": "ITP 438 - Advanced Gameplay Programming",
+        "roles": "Programmer",
+        "page": "itp438.html",
+        "img": "assets/itp438_preview.jpg",
+        "category": ["Game Programming"],
+        "tech": "C++, Unreal Engine 5"
+    },
+    {
+        "name": "Boo's There?",
+        "roles": "Sound Lead",
+        "page": "trickortreat.html",
+        "img": "assets/boosthere_preview.png",
+        "category": ["Audio"],
+        "tech": "C#, Unity 2D, Wwise, Yarn Spinner, Ableton Live"
+    },
+    {
+        "name": "Portal Recreation",
+        "roles": "Programmer",
+        "page": "portalrecreation.html",
+        "img": "assets/portal_preview.png",
+        "category": ["Game Programming"],
+        "tech": "C++, SDL Library"
+    },
+    {
+        "name": "USCheduler",
+        "roles": "Backend Developer",
+        "page": "uscheduler.html",
+        "img": "assets/uscheduler_preview.png",
+        "category": ["SWE"],
+        "tech": "Java, Java Servlets, MySQL Workbench"
+    },
+    {
+        "name": "Hear Your Fear",
+        "roles": "Lead Programmer, Designer, 3D Artist",
+        "page": "hearyourfear.html",
+        "img": "assets/hearyourfear_preview.png",
+        "category": ["Game Programming", "Audio"],
+        "tech": "C#, Unity 3D, Autodesk Maya"
+    },
+    {
+        "name": "Bone Boy",
+        "roles": "Composer, Sound Designer, Audio Programmer",
+        "page": "boneboy.html",
+        "img": "assets/boneboy_preview.png",
+        "category": ["Game Programming", "Audio"],
+        "tech": "C#, Unity 2D, Wwise, Ableton Live"
+    },
+    {
+        "name": "Cellulose",
+        "roles": "Composer, Programmer, UI",
+        "page": "cellulose.html",
+        "img": "assets/cellulose_preview.png",
+        "category": ["Game Programming", "Audio"],
+        "tech": "C#, Unity 2D, Ableton Live"
+    },
+    {
+        "name": "Mom, Please Respect My Privacy",
+        "roles": "Sound Designer, Audio Programmer",
+        "page": "mprmp.html",
+        "img": "assets/mprmp_preview.png",
+        "category": ["Game Programming", "Audio"],
+        "tech": "C#, Unity 2D"
+    },
+    {
+        "name": "Ho Ho Who Did It?",
+        "roles": "Audio Designer",
+        "page": "hhwdi.html",
+        "img": "assets/hhwdi_preview.png",
+        "category": ["Audio"],
+        "tech": "Ableton Live"
+    }
 ];
